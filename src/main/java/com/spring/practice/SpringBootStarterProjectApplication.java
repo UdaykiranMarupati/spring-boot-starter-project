2bd07c047a09a6bdbfe7a2b24c9a41f8fba70a96
UdaykiranReddy250 <marupati.udaykiran@gmail.com>
2017-04-24 21:26:01 -0400

    Jpa Support Added

--- src/main/java/com/spring/practice/SpringBootStarterProjectApplication.java
+++ src/main/java/com/spring/practice/SpringBootStarterProjectApplication.java
@@ -1,7 +1,12 @@
 package com.spring.practice;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.spring.practice.model.Customer;
+import com.spring.practice.model.CustomerRepository;
 
 @SpringBootApplication
 public class SpringBootStarterProjectApplication {
@@ -9,4 +14,22 @@ public class SpringBootStarterProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootStarterProjectApplication.class, args);
 	}
+
+	@Bean
+	public CommandLineRunner demo(CustomerRepository repository) {
+		return (args) -> {
+			// save a couple of customers
+			repository.save(new Customer("Jack", "Bauer"));
+			repository.save(new Customer("Chloe", "O'Brian"));
+			repository.save(new Customer("Kim", "Bauer"));
+			repository.save(new Customer("David", "Palmer"));
+			repository.save(new Customer("Michelle", "Dessler"));
+
+			Customer customer = repository.findOne(1L);
+
+			System.out.println(customer.getFirstName());
+
+		};
+	}
+
 }
