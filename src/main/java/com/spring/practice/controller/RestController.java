2a72c2d70a4226a2c403b52a303672b674c0d892
UdaykiranReddy250 <marupati.udaykiran@gmail.com>
2017-05-03 00:53:31 -0400

    Count method in Spring jpa implemented

--- src/main/java/com/spring/practice/controller/RestController.java
+++ src/main/java/com/spring/practice/controller/RestController.java
@@ -8,18 +8,30 @@ import com.spring.practice.model.PracticeModelRequest;
 import com.spring.practice.model.PracticeModelResponse;
 
 @org.springframework.web.bind.annotation.RestController
-@RequestMapping("/sum")
 public class RestController {
 
-	@RequestMapping(method = RequestMethod.POST)
-	public PracticeModelResponse create(@RequestBody PracticeModelRequest practiceModel) throws com.spring.practice.customerexcetions.BadRequestException {
-		if (practiceModel.getElement1() == null || practiceModel.getElement2() ==null) {
-			throw new  com.spring.practice.customerexcetions.BadRequestException(practiceModel.getElement1() ==null ? "Element1 cannot be null": "Element2 cannot be null");
+	@RequestMapping(value = "/sum", method = RequestMethod.POST)
+	public PracticeModelResponse create(@RequestBody PracticeModelRequest practiceModel)
+			throws com.spring.practice.customerexcetions.BadRequestException {
+		if (practiceModel.getElement1() == null || practiceModel.getElement2() == null) {
+			throw new com.spring.practice.customerexcetions.BadRequestException(
+					practiceModel.getElement1() == null ? "Element1 cannot be null" : "Element2 cannot be null");
 		}
 		PracticeModelResponse response = new PracticeModelResponse();
 		response.setSum(practiceModel.getElement1() + practiceModel.getElement2());
 		return response;
 	}
-	
+
+	@RequestMapping(value = "/count", method = RequestMethod.GET)
+	public PracticeModelResponse countByLastName(@RequestBody PracticeModelRequest practiceModel)
+			throws com.spring.practice.customerexcetions.BadRequestException {
+		if (practiceModel.getElement1() == null || practiceModel.getElement2() == null) {
+			throw new com.spring.practice.customerexcetions.BadRequestException(
+					practiceModel.getElement1() == null ? "Element1 cannot be null" : "Element2 cannot be null");
+		}
+		PracticeModelResponse response = new PracticeModelResponse();
+		response.setSum(practiceModel.getElement1() + practiceModel.getElement2());
+		return response;
+	}
+
 }
-	
