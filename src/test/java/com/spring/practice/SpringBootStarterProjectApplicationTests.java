8e81e2bf1d7208a33610f58b8ef2ea7983ce079a
Uday Kiran <marupati.udaykiran@gmail.com>
2017-05-11 20:32:19 -0500

    test: add test case for SUM post method
    added test case for sum method.

--- src/test/java/com/spring/practice/SpringBootStarterProjectApplicationTests.java
+++ src/test/java/com/spring/practice/SpringBootStarterProjectApplicationTests.java
@@ -10,9 +10,13 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.spring.practice.model.PracticeModelRequest;
 import com.spring.practice.model.PracticeModelResponse;
 
 @RunWith(SpringRunner.class)
@@ -37,6 +41,20 @@ public class SpringBootStarterProjectApplicationTests {
 	public void countTest2() {
 		ResponseEntity<PracticeModelResponse> response = restTemplate
 				.getForEntity("http://localhost:" + port + "/count/Palmer", PracticeModelResponse.class);
+		assertNotNull(response);
+	}
+
+	@Test
+	public void sumTest() {
+
+		PracticeModelRequest request = new PracticeModelRequest();
+		request.setElement1(1);
+		request.setElement2(100);
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		HttpEntity<PracticeModelRequest> entity = new HttpEntity<PracticeModelRequest>(request, headers);
+		ResponseEntity<PracticeModelResponse> response = restTemplate
+				.postForEntity("http://localhost:" + port + "/sum", entity, PracticeModelResponse.class);
 		assertNotNull(response);
 	}
 
