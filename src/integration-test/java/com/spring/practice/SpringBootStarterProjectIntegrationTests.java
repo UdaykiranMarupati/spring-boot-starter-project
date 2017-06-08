1ed586b5b576d73545312b9e1b56636646f16540
Uday Kiran <marupati.udaykiran@gmail.com>
2017-06-07 21:31:56 -0500

    test: implement integration tests
    implemneting integration tests in progress

---
+++ src/integration-test/java/com/spring/practice/SpringBootStarterProjectIntegrationTests.java
@@ -0,0 +1,16 @@
+package com.spring.practice;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@ContextConfiguration(classes = SpringBootStarterProjectApplication.class)
+@SpringBootTest
+public class SpringBootStarterProjectIntegrationTests {
+
+	@Test
+	public void countTest() {
+
+	}
+
+}
